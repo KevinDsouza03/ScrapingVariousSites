01bb9eccb0f39d8511848852b4b87b518717d91f
blurzl <kevin.dsouza@cix.csi.cuny.edu>
2022-12-07 12:58:12 -0500

    Pacsun scraper added, but proxy support is needed. Work on webshare api

--- readstylevana.py
+++ readstylevana.py
@@ -4,7 +4,7 @@ import functionheaders
 urls = []
 url = 'https://www.stylevana.com/en_US/promotion/trending-brands.html?dir=desc&limit=72&order=popularity&price=0-10.01'
 
-
+"""
 functionheaders.parse_link(url,urls)
 for x in range(2,6,1):
     url_front = "https://www.stylevana.com/en_US/promotion/trending-brands.html?dir=desc&limit=72&order=popularity&p="
@@ -12,7 +12,7 @@ for x in range(2,6,1):
     url_middle = str(x+1)
     url = url_front+url_middle+url_back
     functionheaders.parse_link(url,urls)
-
+"""
 """
 url = 'https://www.stylevana.com/en_US/skincare.html?limit=72&price=3.75-10.01'
 functionheaders.parse_link(url,urls)
@@ -23,5 +23,8 @@ for x in range(2,24,1): #24 pages on the skincare s
     url = url_front+url_middle+url_back
     functionheaders.parse_link(url,urls)
 """
-#print(*urls, sep="\n")
-functionheaders.write_to_file('itemsResults\PopItems.txt',urls)
+url = 'https://www.pacsun.com/mens/sale/'
+#functionheaders.proxy_get('ecfhgyaxz002dqdb29uefx90cf0ujy0y0qerp2cf')
+functionheaders.parse_pacsun(url,urls)
+print(*urls, sep="\n")
+functionheaders.write_to_file('itemsResults\pacsunMensSale.txt',urls)
