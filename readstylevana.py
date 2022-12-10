cc78fcc3a02073f0c6a5cda57e36106ab8c41167
KevinDsouza03 <dsouzakev467@gmail.com>
2022-12-09 19:31:14 -0500

    Attempted to have object for writing. fail can write to csv but ran into bot protection/throttling removed pacsun, will focus on stylevana alone for now

--- readstylevana.py
+++ readstylevana.py
@@ -1,9 +1,9 @@
 import functionheaders
- 
+import time
 #url = 'https://www.stylevana.com/en_US/promotion/flash-deals.html?dir=desc&limit=72&order=popularity&price=0-10'
 urls = []
 url = 'https://www.stylevana.com/en_US/promotion/trending-brands.html?dir=desc&limit=72&order=popularity&price=0-10.01'
-
+header_list = ['Title','Original Price','Link']
 """
 functionheaders.parse_link(url,urls)
 for x in range(2,6,1):
@@ -13,18 +13,25 @@ for x in range(2,6,1):
     url = url_front+url_middle+url_back
     functionheaders.parse_link(url,urls)
 """
-"""
+start_time = time.time()
 url = 'https://www.stylevana.com/en_US/skincare.html?limit=72&price=3.75-10.01'
+
 functionheaders.parse_link(url,urls)
-for x in range(2,24,1): #24 pages on the skincare s
+for x in range(2,24,1): #24 pages on the skincare 
     url_front = "https://www.stylevana.com/en_US/skincare.html?limit=72&p="
     url_back = "&price=3.75-10.01"
     url_middle = str(x+1)
     url = url_front+url_middle+url_back
     functionheaders.parse_link(url,urls)
-"""
-url = 'https://www.pacsun.com/mens/sale/'
-#functionheaders.proxy_get('ecfhgyaxz002dqdb29uefx90cf0ujy0y0qerp2cf')
-functionheaders.parse_pacsun(url,urls)
-print(*urls, sep="\n")
-functionheaders.write_to_file('itemsResults\pacsunMensSale.txt',urls)
+
+end_time = time.time()
+elapsed_time = end_time-start_time
+
+print (f"Time to parse: {elapsed_time}")
+print(len(urls))
+#print(*urls, sep="\n")
+start_time = time.time()
+functionheaders.write_to_csv('itemsResults\skincareResults.csv',urls,header_list)
+end_time = time.time()
+elapsed_time = end_time-start_time
+print (f"Time to write: {elapsed_time}")
